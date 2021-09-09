29608ef3f900a92ebc18ef95f9713aea1fb253da
Veronica Mangiaterra <89794523+veronica-mangiaterra@users.noreply.github.com>
2021-09-09 12:26:55 +0200

    Update and rename training.py to svm-setup-9-fold-cross-validation.py

--- svm-setup-9-fold-cross-validation.py
+++ svm-setup-9-fold-cross-validation.py
@@ -1,20 +1,6 @@
-import os
-from sys import argv
-
-from numpy import concatenate
-from sklearn.svm import SVC
-from sklearn.model_selection import cross_val_score
-from sklearn.model_selection import cross_val_predict
-from sklearn.model_selection import train_test_split
-
-
-#print(args)
-C       = 100
-kernel  = 'linear'
-degree  =  3
 
 #csv sources
-files = [r'C:\Users\user\Documents\1m\class1\vecs.csv',r'C:\Users\user\Documents\1m\class2\vecs.csv']
+files = [r'path1',r'path2']
 
 #fetch data in csv and structure in a class:vector dict
 cl_dict = get_data(files)
@@ -24,8 +10,6 @@ t2 = list(cl_dict.keys())[1]
 data = list(cl_dict[t1].values())+list(cl_dict[t2].values())
 my_vectors = np. array(data)
 my_labels = make_labels(len(list(cl_dict[t1].values())), len(list(cl_dict[t2].values())))
-# tr_vectors, te_vectors, tr_label, te_label = train_test_split(
-#          my_vectors, my_labels, test_size=0.1)
 
 #setup SVM setup and print output
 print('SVC output:')
